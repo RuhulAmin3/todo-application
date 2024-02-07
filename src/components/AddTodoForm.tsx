8c39055bda2b3a9513d6f288256833b374c44a42
RuhulAmin3 <dev.ruhulamin3@gmail.com>
2024-02-07 19:51:42 +0600

    todo application initialized

---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,10 @@
+const AddTodoForm = () => {
+  return (
+    <form>
+      <input type="text" placeholder="title" />
+      <input type="text" placeholder="details" />
+    </form>
+  );
+};
+
+export default AddTodoForm;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="text-white bg-[#2B1887] h-1/2 w-1/2 m-auto">
+      <h3 className="text-xl font-medium p-4">Todo Application </h3>
+    </div>
+  );
+};
+
+export default Home;
